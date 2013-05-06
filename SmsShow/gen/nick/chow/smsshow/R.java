77b32a2fbfc9a4e27f76341a20503261ad26c156
yunchow <yunchow@126.com>
2013-05-07 00:08:16 +0800

    refact directory

--- SmsShow/gen/nick/chow/smsshow/R.java
+++ SmsShow/gen/nick/chow/smsshow/R.java
@@ -15,8 +15,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int button_selector=0x7f080002;
         public static final int clolor_369=0x7f080000;
         public static final int clolor_DDD=0x7f080001;
+        public static final int list_items_bg=0x7f080003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -29,13 +31,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int button_selector=0x7f020000;
-        public static final int buttonstyle=0x7f020001;
-        public static final int corner=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int list_items_bg=0x7f020004;
-        public static final int listviewstyle=0x7f020005;
-        public static final int mystyle=0x7f020006;
+        public static final int buttonstyle=0x7f020000;
+        public static final int corner=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int listviewstyle=0x7f020003;
+        public static final int mystyle=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0008;
