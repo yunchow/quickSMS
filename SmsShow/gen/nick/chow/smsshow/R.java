dbbad0c993169cb78cda25f6580db845396db146
yunchow <yunchow@126.com>
2013-05-24 01:35:28 +0800

    update UI

--- SmsShow/gen/nick/chow/smsshow/R.java
+++ SmsShow/gen/nick/chow/smsshow/R.java
@@ -48,11 +48,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_cab_done_holo_light=0x7f020000;
-        public static final int ic_delete=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int indicator_input_error=0x7f020003;
-        public static final int state_notify_msg_orange_original=0x7f020004;
+        public static final int arrow_bottom_transparent=0x7f020000;
+        public static final int dialog_divider_part=0x7f020001;
+        public static final int group_divider=0x7f020002;
+        public static final int ic_cab_done_holo_light=0x7f020003;
+        public static final int ic_delete=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int indicator_input_error=0x7f020006;
+        public static final int state_notify_msg_orange_original=0x7f020007;
+        public static final int tab_icon_contact_selected=0x7f020008;
     }
     public static final class id {
         public static final int action_about=0x7f0c0019;
